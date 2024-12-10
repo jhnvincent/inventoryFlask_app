5d4f7db195f02a2cefd63a8a2c2e9fd7e77c7d01
jhnvincent <labotoyjohnvincent@gmail.com>
2024-12-10 23:14:17 +0800

    add the models from the database

--- myapp.py
+++ myapp.py
@@ -6,3 +6,49 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:JV2047labs.?@localhost/inventory'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+db = SQLAlchemy(app)
+
+#This is models part
+class Customer(db.Model):
+    __tablename__ = 'customers'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)  
+    role = db.Column(db.String(50), nullable=False, default='customer') 
+    orders = db.relationship('Order', backref='customer', lazy=True)
+
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    product_name = db.Column(db.String(100), nullable=False)
+    unit_price = db.Column(db.Numeric(10, 2), nullable=False)
+    stock_quantity = db.Column(db.Integer, nullable=False, default=0)
+
+    transactions = db.relationship('InventoryTransaction', backref='product', lazy=True)
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'), nullable=False)
+    date_of_order = db.Column(db.Date, nullable=False)
+    items = db.relationship('OrderItem', backref='order', lazy=True)
+
+class OrderItem(db.Model):
+    __tablename__ = 'order_items'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    product_quantity = db.Column(db.Integer, nullable=False)
+    product = db.relationship('Product', backref='order_items', lazy=True)
+
+
+class InventoryTransaction(db.Model):
+    __tablename__ = 'inventory_transactions'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'), nullable=False)
+    transaction_type = db.Column(Enum('IN', 'OUT', name='transaction_type_enum'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    transaction_date = db.Column(db.Date, nullable=False)
